3f724d7ed5834441e1494f7f6a86811102e7df21
Tim Moreton <tmoreton89@gmail.com>
2015-03-19 17:57:03 -0400

    added models

---
+++ server/db/models/product.js
@@ -0,0 +1,29 @@
+'use strict';
+var mongoose = require('mongoose');
+var unique = require('mongoose-unique-validator');
+
+var schema = new mongoose.Schema({
+    photoUrl: {
+        type: String        
+    },
+    photo: {
+        data: Buffer, // should come back 
+        contentType: String
+    },
+    description: {
+        type: String
+    },
+    expiration: {
+        type: Date, 
+        default: new Date()
+    },
+    location: {
+        type: String
+    }
+});
+
+
+var Product = mongoose.model('Product', schema);
+module.exports = {Product: Product};
+
+
