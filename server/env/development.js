72a3e1a2cb25c976b10f3d415437fe59e7f55d32
Tim Moreton <tmoreton89@gmail.com>
2015-03-25 11:04:38 -0400

    added mongolabs

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/free-swap",
+  "DATABASE_URI": "mongodb://localhost/free-swap",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
