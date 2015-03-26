7581effa2f390c19e7fe03e51a8eaaf603f11fe3
CarpeDN <diennguyen21@hotmail.com>
2015-03-26 17:36:45 -0400

    login works

--- browser/js/front-page/front-page.js
+++ browser/js/front-page/front-page.js
@@ -2,7 +2,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
     .state('front-page', {
-    url: "/front-page",
+    url: '/front-page',
     templateUrl: "/js/front-page/front-page.html",
     controller: 'FrontPageCtrl'
   })
@@ -11,16 +11,21 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.controller('FrontPageCtrl', function($scope, $window, $location, $state, user) {
 
   $scope.signUp = function(newUser) {
-    user.createUser(newUser).then(function() {
-      $state.go('app.swap');
-    });
+    user.createUser(newUser);
+    $scope.newUser = {};
+  };
+
+  $scope.loginPlatform = function(platform) {
+    user.loginSocial(platform);
   }
 
   // Login not working... get this fixed
   $scope.login = function(credentials) {
     user.login(credentials).then(function(user) {
-      console.log('Logged in user',user);
-      // $state.go('app.swap'); // Make this route only work if login is validated...
+      if (user) {
+        console.log('Logged in user',user);
+        $state.go('app.swap');
+      }
     })
   };
 })
